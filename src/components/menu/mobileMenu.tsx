37a638fa3e45eef486e7babf89c576dd60906561
trandat1114 <cctalk1114@gmail.com>
2023-08-18 17:28:12 +0700

    JayAndy-Portfolio

--- src/components/menu/mobileMenu.tsx
+++ src/components/menu/mobileMenu.tsx
@@ -1,6 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Sheet,
   SheetClose,
@@ -12,52 +10,118 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faPhone, faEnvelope, faCopyright } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
 
-const currentTime = new Date();
-const currentHour = currentTime.getHours();
-let greeting = "";
 
-if (currentHour < 12) {
-  greeting = "Good morning";
-} else if (currentHour < 18) {
-  greeting = "Good afternoon";
-} else {
-  greeting = "Good evening";
+interface navContent {
+  title: string;
+  href: string;
+  icon?: any;
 }
 
 export function MobileMenu() {
+  //Time
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let greeting = "";
+
+  if (currentHour < 12) {
+    greeting = "🌄 Good morning";
+  } else if (currentHour < 18) {
+    greeting = "🌇 Good afternoon";
+  } else {
+    greeting = "🌃 Good evening";
+  }
+
+  //Contact link
+  const ListNavContents: navContent[] = [
+    {
+      title: "Phone Number",
+      href: "tel:+84347647856",
+      icon: faPhone,
+    },
+    {
+      title: "Email",
+      href: "mailto:dattranphu1114@gmail.com",
+      icon: faEnvelope
+    },
+    {
+      title: "Twitter",
+      href: "https://twitter.com/DatTranM4",
+      icon: faTwitter
+    },
+    {
+      title: "Github",
+      href: "https://github.com/TranDat1114",
+      icon: faGithub
+    },
+    {
+      title: "LinkedIn",
+      href: "https://www.linkedin.com/in/tran-phu-dat-526a82288",
+      icon: faLinkedin
+    }
+  ]
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button className="px-4">
-          <FontAwesomeIcon className="w-[24px]" icon={faBars}/>
+        <Button className="p-4">
+          <FontAwesomeIcon className="w-[16px]" icon={faBars} />
         </Button>
       </SheetTrigger>
       <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Jay Andy Navigation bar</SheetTitle>
-          <SheetDescription>
-          {greeting}, how are you doing? I just wanted to say hi and let you know that I am thinking of you. You are a wonderful person and a great friend. I hope you are happy and healthy.
+        <SheetHeader className="text-left">
+          <SheetTitle>Jay Andy</SheetTitle>
+          <SheetDescription className="text-primary font-light italic">
+            {greeting}, how are you doing?
+            <p>
+              I just wanted to say hi and let you know that you are a wonderful person and a great friend.
+            </p>
+            <Separator className="my-4" />
           </SheetDescription>
         </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <div className="w-full">
+          <div id="Link-section-1" className="flex flex-col space-y-2">
+            <Link href={"/About"}>
+              <p className="text-lg text-foreground">About</p>
+            </Link>
+            <Link href={"/Portfolio"}>
+              <p className="text-lg text-foreground">Portfolio</p>
+            </Link>
+            <Link href={"/Blog"}>
+              <p className="text-lg text-foreground">Blog</p>
+            </Link>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+          <Separator className="my-2 w-9/12" />
+          <div id="Contact-section-1">
+            <p className="text-lg text-foreground">Contact</p>
+            <div className="grid grid-col items-center justify-start gap-2 w-full px-2">
+              {ListNavContents.map((navItem, index) => (
+                <Link className="text-left" key={index} href={navItem.href} target="_blank">
+                  <div className="grid grid-flow-row grid-cols-6 gap-4 items-center">
+                    <FontAwesomeIcon className="w-[16px] text-primary col-span-1" icon={navItem.icon} />
+                    <p className="col-span-5 text-opacity-75 text-muted-foreground whitespace-nowrap">
+                      {navItem.title}
+                    </p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <Separator className="my-4" />
+          <div className="fixed bottom-0 flex flex-row space-x-2 items-center">
+            <FontAwesomeIcon className="w-[16px] text-primary" icon={faCopyright} />
+            <Link href={"https://twitter.com/DatTranM4"} className="text-muted-foreground whitespace-nowrap" target="_blank">
+              Tran Phu Dat - 2023</Link>
           </div>
         </div>
         <SheetFooter>
           <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
+            <p className="text-base whitespace-nowrap text-primary font-light italic">
+              💕I hope you are happy today😊.
+            </p>
           </SheetClose>
         </SheetFooter>
       </SheetContent>
