71ff31d2f0c329af40a01411d78c522b1628c658
trandat1114 <cctalk1114@gmail.com>
2023-08-21 05:19:10 +0700

    nice

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
 
     return (
         //Desktop and tablet header
-        <header className="sticky backdrop-blur-md z-50 border-b p-2 top-0 max-h-20 w-ful">
+        <header className="sticky backdrop-blur-lg z-50 border-b border-current p-2 top-0 max-h-20 w-ful">
             <div className="container flex h-14 flex-row justify-between items-center">
                 <Link className="text-3xl font-extrabold tracking-tighter lg:text-5xl underline shadow-transparent drop-shadow-md" href="/">Jay Andy</Link>
                 <nav className="desktop-menu"></nav>
